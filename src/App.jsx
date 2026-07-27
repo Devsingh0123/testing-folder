6d0edc4e5b30a7ea4e2dbea7a2dbd27176527089
Devsingh0123 <dayamns20@gmail.com>
2026-07-27 18:10:40 +0530

    test file input and making frontend url

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [form, setForm] = useState({ myname: "" });
 
-export default App
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const files = e.target.files;
+    setForm({ ...form, [name]: value });
+    const filesArray = [...files];
+    console.log("files", files);
+    console.log("filesArray", filesArray);
+
+    const generatedUrl = URL.createObjectURL(files[0]); 
+    console.log(generatedUrl)
+  };
+
+  console.log(form);
+
+  const obj = ["daya", "rahul"];
+
+  console.log(...obj);
+  return (
+    <div>
+      <input type="text" name="myname" onChange={handleChange} />
+      <input type="file" multiple onChange={handleChange} />
+    </div>
+  );
+};
+
+export default App;
